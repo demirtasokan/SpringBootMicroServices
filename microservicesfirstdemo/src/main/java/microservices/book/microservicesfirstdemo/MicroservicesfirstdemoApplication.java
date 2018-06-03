f4b11d727e5067444bea5a74ae28333cc10b286d
Okan Demirtaş <demirtasokan@gmail.com>
2018-06-03 18:17:09 +0300

    first commit for microservices

---
+++ microservicesfirstdemo/src/main/java/microservices/book/microservicesfirstdemo/MicroservicesfirstdemoApplication.java
@@ -0,0 +1,12 @@
+package microservices.book.microservicesfirstdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesfirstdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesfirstdemoApplication.class, args);
+	}
+}
---
+++ microservicesfirstdemo/src/test/java/microservices/book/microservicesfirstdemo/MicroservicesfirstdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package microservices.book.microservicesfirstdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroservicesfirstdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
